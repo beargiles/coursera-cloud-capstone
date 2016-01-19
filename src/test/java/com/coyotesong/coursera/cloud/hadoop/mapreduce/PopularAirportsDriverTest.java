3109964fec2f4875255f83693a5ee8b3e0126889
Bear Giles <bgiles@coyotesong.com>
2016-01-18 21:28:43 -0700

    Removed unused imports.

--- src/test/java/com/coyotesong/coursera/cloud/hadoop/mapreduce/PopularAirportsDriverTest.java
+++ src/test/java/com/coyotesong/coursera/cloud/hadoop/mapreduce/PopularAirportsDriverTest.java
@@ -12,10 +12,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
